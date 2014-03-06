9f8484e16bf0c5d96612d18aef2814745cd317e5
Lucas Augusto Cordeiro <lucas.cordeiro@msn.com>
2014-03-06 11:33:18 -0300

    Tirinha view controller

---
+++ Comics Maker/OCTirinhaViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCTirinhaViewController.h
+//  Comics Maker
+//
+//  Created by Lucas Augusto Cordeiro on 3/6/14.
+//  Copyright (c) 2014 Lucas Augusto Cordeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCTirinhaViewController : UIViewController
+
+@end
---
+++ Comics Maker/OCTirinhaViewController.m
@@ -0,0 +1,38 @@
+//
+//  OCTirinhaViewController.m
+//  Comics Maker
+//
+//  Created by Lucas Augusto Cordeiro on 3/6/14.
+//  Copyright (c) 2014 Lucas Augusto Cordeiro. All rights reserved.
+//
+
+#import "OCTirinhaViewController.h"
+
+@interface OCTirinhaViewController ()
+
+@end
+
+@implementation OCTirinhaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
